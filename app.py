1364ff1725b2d4c3a133a98b79515f4bb32ced9d
USER <arjunnambiar58@gmail.com>
2025-04-23 19:07:57 +0530

    🎯 Test full CI/CD deploy

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask CI/CD app!"
+    return "Hello from DevOps pipeline!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
